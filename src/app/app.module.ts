5fd445f46aae22f2e3eac3f8c52c313d8cc3f1a6
ottovbismarck <fritzj96@gmail.com>
2018-08-14 16:46:22 -0700

    RIP all who try to figure out what this commit was for, no idea myself.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PropCalcComponent } from './prop-calc/prop-calc.component';
-import { FormComponent } from './prop-calc/form/form.component';
 import { FormsModule } from '../../node_modules/@angular/forms';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { FormsModule } from '../../node_modules/@angular/forms';
     AppComponent,
     NavbarComponent,
     PropCalcComponent,
-    FormComponent
   ],
   imports: [
     BrowserModule,
